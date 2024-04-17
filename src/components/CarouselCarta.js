f5ad1ce1146237e576486ba6e2cd5adf275b2d6f
LunaticBandit <lunachristian696@gmail.com>
2024-04-17 10:34:49 -0600

    Código a 17 de abril de 2024

--- src/components/CarouselCarta.js
+++ src/components/CarouselCarta.js
@@ -1,34 +1,64 @@
-import React from "react"
+
+import React, { useState } from "react"
+import { Carousel, Row, Col, Card, Button } from "react-bootstrap"
 import "bootstrap/dist/css/bootstrap.min.css"
-import { Carousel, Card, Button, Row, Col } from "react-bootstrap"
+import "./CarouselCartaStyles.css"
 
 const CarouselCarta = ({ cardData }) => {
-  // Llenar el array cardData con elementos duplicados o circulares para asegurar que siempre haya suficientes elementos para mostrar 3 cartas en el carousel
+  const [showSplitView, setShowSplitView] = useState(false);
+  const [selectedCard, setSelectedCard] = useState(null);
+
+  const openSplitView = (card) => {
+    setSelectedCard(card);
+    setShowSplitView(true);
+  };
+
+  const closeSplitView = () => {
+    setShowSplitView(false);
+    setSelectedCard(null);
+  };
+
   const filledData = [...cardData, ...cardData, ...cardData];
 
   return (
-    <Carousel interval={null} indicators={false} style={{ marginBottom: "20px", background:"#204294"}}>
-      {filledData.map((_, index) => (
-        index % 3 === 0 && (
-          <Carousel.Item key={index} style={{ margin:"25px 0 25px 100px",width:"85%"}}>
-            <Row>
-              {filledData.slice(index, index + 3).map((card, i) => (
-                <Col key={i} sm={4}>
-                  <Card style={{width: "18rem" }}>
-                    <Card.Img variant="top" src={card.img} alt={card.title} />
-                    <Card.Body>
-                      <Card.Title>{card.title}</Card.Title>
-                      <Card.Subtitle style={{ color: "black" }}>{card.nombre}</Card.Subtitle>
-                      <Button variant="primary">Ver más</Button>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              ))}
-            </Row>
-          </Carousel.Item>
-        )
-      ))}
-    </Carousel>
+    <>
+      <Carousel interval={null} indicators={false} style={{ marginBottom: "20px", background:"linear-gradient(to bottom right,#1e3c72 0%, #1e3c72 1%, #2a5298 100%, white)"}}>
+        {filledData.map((_, index) => (
+          index % 3 === 0 && (
+            <Carousel.Item key={index} style={{ margin:"25px 0 25px 100px",width:"85%"}}>
+              <Row>
+                {filledData.slice(index, index + 3).map((card, i) => (
+                  <Col key={i} sm={4}>
+                    <Card style={{width: "18rem" }}>
+                      <Card.Img variant="top" src={card.img} alt={card.title} />
+                      <Card.Body>
+                        <Card.Title>{card.title}</Card.Title>
+                        <Card.Subtitle style={{ color: "black", margin:"10px 0" }}>{card.nombre}</Card.Subtitle>
+                        <Button style={{position:"relative", left:"25%", marginTop:"5px"}} onClick={() => openSplitView(card)}>Ver más</Button>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+            </Carousel.Item>
+          )
+        ))}
+      </Carousel>
+
+      {showSplitView && (
+        <div className="carta-ventana">
+          
+          <div className="ventana-izq">
+            <img src={selectedCard.img} alt={selectedCard.title} />
+          </div>
+          <div className="ventana-der">
+            <h2 className="titulo ">{selectedCard.title}</h2>
+            <p className="texto">{selectedCard.info}</p>
+            <Button className="close-button" onClick={closeSplitView}>Cerrar</Button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- src/routes/Directivos.js
+++ src/routes/Directivos.js
@@ -10,37 +10,55 @@ import ImgTesorera from "../assets/imagesDirectivos/imgTesorera.jpg"
 import "./StylesRoutes.css"
 
 export default function Directivos() {
-
+  
   const carta = [
     {
       img: ImgPresidente,
       title: "Presidente",
       nombre: "Rafael Fernández Corro",
+      info: `Rafael Fernández Corro es Ingeniero en Electrónica y Telecomunicaciones por la Universidad Iberoamericana, 
+      con estudios de Dirección de empresas en el IPADE y en Wharton.
+      Actualmente es VP Latinoamérica de BT (British Telecom),
+      Rafael es Presidente de la Academia Mexicana de Informática, A.C. (AMIAC),
+      adicionalmente es miembro del consejo de la Cámara Británica de Comercio en
+      México, y presidente del grupo de Transformación digital de la misma Cámara.
+      Se desempeñó como Presidente de la Asociación de egresados de su carrera en la
+      Universidad Iberoamericana, de la que también fue profesor de la materia
+      “Redes de computadoras” durante más de diez años.
+      Ha sido el responsable para México y Latinoamérica de varias empresas de
+      tecnologías de Información y Comunicaciones, como 3Com, Nortel, Avaya y
+      ShoreTel, contribuyendo al desarrollo del sector en México.
+      Es editor del portal www.vinculotic.com que se encarga de difundir
+      experiencias, casos de éxito y tendencias de la industria TIC en los segmentos de
+      Educación y Salud.`
     },
    
     {
       img: ImgVicepresidenta,
       title: "Vicepresidenta",
       nombre: "Alejandra Herrera",
+      info: "2"
     }, 
     
     {
       img: ImgSecretario,
       title: "Secretario",
       nombre: "José Carlos Morales",
-
+      info: "3"
     },
     
     {
       img: ImgSecrVinculacion,
       title: "Secretaria de Vinculación",
       nombre: "Julieta Palma Anda",
+      info: "4"
     },
 
     {
       img: ImgTesorera,
       title: "Tesorera",
       nombre: "Alejandra Pineda",
+      info: "5"
     },    
   ]
 
